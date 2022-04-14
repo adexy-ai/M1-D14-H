00000da9c75f35e2359c78ef71429210d72a96ae
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-14 19:19:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,43 +17,54 @@ console.log(myObject)
 delete myObject.email
 console.log(myObject)
 //         24) Create an array with 10 strings in it
-let myArray = ["one", "two", "three", "four", "five", "six", "seven", "8", "9", "10"]
+let myArray = []
 
+for (let i = 0; i < 10; i++){
+    myArray.push("Hello World")
+}
 //         25) Print in the console every string from the previous array
 console.log(myArray)
 
-//         26) Create an array with 100 random numbers in it
-let randomArray  = [];
-for (var i = 0, n = 100; i < n; i++) {
-    randomArray.push(Math.round(Math.random() * n))
+// 26) Create an aray with 100 random numbers in it
+let randomArray = [];
+let n = 100;
+for (let i = 0; i < n; i++) {
+randomArray.push(Math.round(Math.random() * n))
 }
 console.log(randomArray)
-//         27) Write a function to get the maximum and minimum values from the previously created array
+// //         27) Write a function to get the maximum and minimum values from the previously created array
 
-function maxArray() {
+// function maxArray() {
      
-}
+// }
  
-function minArray() {
+// function minArray() {
 
-}
-//         28) Create an array of arrays, in which every array has 10 random numbers
-function arrayOfArray() = {
-    let ranArray = [];
-    n = 10;
-    for (var i = 0; i < n; i++) {
-        array = ranArray.push(Math.round(Math.random() * n))
-        return array
-    }
+// }
+// //         28) Create an array of arrays, in which every array has 10 random numbers
+// function arrayOfArray() = {
+//     let ranArray = [];
+//     n = 10;
+//     for (var i = 0; i < n; i++) {
+//         array = ranArray.push(Math.round(Math.random() * n))
+//         return array
+//     }
 
-    arrayOfArray()
-//         29) Create a function that gets 2 arrays as parameters and returns the longest one
-    function twoArray(a, b) {
-        let thisArray = [];
-        for 
+//     arrayOfArray()
+// //         29) Create a function that gets 2 arrays as parameters and returns the longest one
+//     function twoArray(a, b) {
+//         let myArray = [];
+//         for     
+//     }
+// //         30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+//     function arrayOfTwoNumbers(a, b) {
+//         let twoArray = []
+//         for (i = 0; i < twoArray.length; i++) 
+    
+//     }
+    
 
-    }
-//         30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+//     }
 
 
 
