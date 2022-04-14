bfbdbed24c6a68bca7675d7ae6d2e5c0189cbf4f
Aderiye <69057307+adexy-ai@users.noreply.github.com>
2022-04-14 18:01:45 +0300

    trying out the js assignment

---
+++ script.js
@@ -0,0 +1,67 @@
+// JS EXERCISES
+
+//         21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
+let x = "John"
+let y = "Doe"
+console.log('"' + x + " " + "<>" + " " + y + '"')
+//         22) Create an object with properties such name, surname, email
+
+let myObject = {
+    name: "Aderiye",
+    surname: "Adeniran",
+    email: "nebej@gmail.com",
+    phone: +3586388390
+}
+console.log(myObject)
+//         23) Delete the email property from the previously created object
+delete myObject.email
+console.log(myObject)
+//         24) Create an array with 10 strings in it
+let myArray = ["one", "two", "three", "four", "five", "six", "seven", "8", "9", "10"]
+
+//         25) Print in the console every string from the previous array
+console.log(myArray)
+
+//         26) Create an array with 100 random numbers in it
+let randomArray  = [];
+for (var i = 0, n = 100; i < n; i++) {
+    randomArray.push(Math.round(Math.random() * n))
+}
+console.log(randomArray)
+//         27) Write a function to get the maximum and minimum values from the previously created array
+
+function maxArray() {
+     
+}
+ 
+function minArray() {
+
+}
+//         28) Create an array of arrays, in which every array has 10 random numbers
+function arrayOfArray() = {
+    let ranArray = [];
+    n = 10;
+    for (var i = 0; i < n; i++) {
+        array = ranArray.push(Math.round(Math.random() * n))
+        return array
+    }
+
+    arrayOfArray()
+//         29) Create a function that gets 2 arrays as parameters and returns the longest one
+    function twoArray(a, b) {
+        let thisArray = [];
+        for 
+
+    }
+//         30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+
+
+
+
+
+
+
+
+
+
+
